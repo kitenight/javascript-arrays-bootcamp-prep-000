6f3482328177bde97f5a656bd1e53cb9700abcbc
Lyn <jen.l@mailfence.com>
2018-09-19 08:30:59 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat","skittles"];
+return chocolateBars;
+
+   
+function addElementToBeginningOfArray ("array", "element")
+["object"...addElementToBeginningOfArray];
+addElementToBeginningOfArray = ["object"...addElementToBeginningOfArray];
+
+function destructivelyAddElementToBeginningOfArray ("array", "element")
